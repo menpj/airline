e19e71027e4d942e05b8c4e3f28b31239100106b
menpj <3994084+menpj@users.noreply.github.com>
2024-07-16 14:30:32 +0530

    first commit

---
+++ flights/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+
+from .models import Flight,Passenger,Airport
+
+from django.http import HttpResponseRedirect, Http404   
+from django.urls import reverse
+
+# Create your views here.
+def index(request):
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all()
+    })
+
+def flight(request, flight_id):
+    try:
+        flight=Flight.objects.get(id=flight_id)
+    except Flight.DoesNotExist:
+        raise Http404("Flight Not Found")
+    passengers = flight.passengers.all()
+    non_passengers = Passenger.objects.exclude(flights=flight).all()
+    for passenger in passengers:
+        print(passenger)
+    return render(request, "flights/flight.html", {
+        "flight": flight,
+        "passengers": passengers,
+        "non_passengers":non_passengers
+
+    })
+
+
+def book(request,flight_id):
+    if request.method=="POST":
+
+        flight = Flight.objects.get(pk=flight_id)
+
+        passenger_id = int(request.POST["passenger"])
+
+        passenger = Passenger.objects.get(pk=passenger_id)
+        passenger.flights.add(flight)
+        
+        return HttpResponseRedirect(reverse("flight", args = (flight.id,)))
